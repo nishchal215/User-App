ea0c5274ca39bcd2231fcae10d321dd890805cd7
Nishchal Prakash <nispra39@gmail.com>
2020-02-07 18:13:37 +0530

    Face Recognition implemented in SignUp Page

--- app/src/main/java/com/example/userapp/SignUpActivity.java
+++ app/src/main/java/com/example/userapp/SignUpActivity.java
@@ -11,6 +11,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +26,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -32,11 +37,15 @@ import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
 
     EditText emaiId, name, password, password2;
     CircleImageView profilePicture;
@@ -44,6 +53,10 @@ public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     StorageReference storageReference;
     String txt_email, txt_name, txt_password, txt_password2, txt_imageURL;
+    Uri resultUri = null;
+
+    FirebaseVisionImage image;
+    int numberOfFaces, flag=0;
 
 
     @Override
@@ -77,18 +90,62 @@ public class SignUpActivity extends AppCompatActivity {
                 txt_password = password.getText().toString();
                 txt_password2 = password2.getText().toString();
 
-                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_name) || TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(txt_password2)) {
+                if(txt_email.trim().equalsIgnoreCase("")){
 
-                    Toast.makeText(SignUpActivity.this, "Field(s) cannot be empty!", Toast.LENGTH_SHORT).show();
+                    emaiId.setError("This field cannot be empty!");
+                    flag = 1;
 
                 }
-                if (!TextUtils.equals(txt_password, txt_password2)) {
+
+
+                if(txt_name.trim().equalsIgnoreCase("")){
+
+                    name.setError("This field cannot be empty!");
+                    flag = 1;
+
+                }
+
+
+                if(txt_password.trim().equalsIgnoreCase("")){
+
+                    password.setError("This field cannot be empty!");
+                    flag = 1;
+
+                }
+
+
+                if(txt_password2.trim().equalsIgnoreCase("")){
+
+                    password2.setError("This field cannot be empty!");
+                    flag = 1;
+
+                }
+
+                if(resultUri == null){
+
+                    Toast.makeText(SignUpActivity.this, "Choose a proper Profile Picture", Toast.LENGTH_SHORT).show();
+                    flag = 1;
+
+                }
+
+//                if (txt_email.equalsIgnoreCase("") || TextUtils.isEmpty(txt_name) || TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(txt_password2)) {
+//
+//                    Toast.makeText(SignUpActivity.this, "Field(s) cannot be empty!", Toast.LENGTH_SHORT).show();
+//
+//                }
+                if (!password.equals(password2) && !txt_password.equals("") && !txt_password2.equals("")) {
                     Toast.makeText(SignUpActivity.this, "Passwords don't match", Toast.LENGTH_SHORT).show();
                     password.setText("");
                     password2.setText("");
-                } else {
-                    register(txt_email, txt_password);
+                    flag = 1;
                 }
+
+                if(flag == 0){
+
+                    register(txt_email, txt_password);
+
+                }
+
 
             }
         });
@@ -118,6 +175,8 @@ public class SignUpActivity extends AppCompatActivity {
 
                     FirebaseUser currentUser = mAuth.getCurrentUser();
                     String userId = currentUser.getUid();
+
+                    uploadImage(resultUri);
 
                     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
 
@@ -158,9 +217,70 @@ public class SignUpActivity extends AppCompatActivity {
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
-                Uri resultUri = result.getUri();
+                resultUri = result.getUri();
 
-                uploadImage(resultUri);
+                final ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.setMessage("Checking...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+
+
+                if(resultUri != null) {
+
+                    try {
+                        image = FirebaseVisionImage.fromFilePath(getApplicationContext(), resultUri);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    FirebaseVisionFaceDetector detector = FirebaseVision.getInstance().getVisionFaceDetector();
+
+                    Task<List<FirebaseVisionFace>> taskResult =
+                            detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionFace>>() {
+                                @Override
+                                public void onSuccess(List<FirebaseVisionFace> firebaseVisionFaces) {
+
+//                                    progressDialog.dismiss();
+//                                    Toast.makeText(SignUpActivity.this, ""+firebaseVisionFaces.size(), Toast.LENGTH_SHORT).show();
+                                    numberOfFaces = firebaseVisionFaces.size();
+
+                                    if(numberOfFaces == 1){
+
+                                        profilePicture.setImageURI(resultUri);
+
+                                    }else if(numberOfFaces > 1){
+
+                                        resultUri = null;
+                                        Toast.makeText(SignUpActivity.this, "More than 1 faces detected. Choose a new photo containing only your face.", Toast.LENGTH_SHORT).show();
+
+                                    }else{
+
+                                        resultUri = null;
+                                        Toast.makeText(SignUpActivity.this, "No faces detected. Choose a photo of your face.", Toast.LENGTH_SHORT).show();
+
+                                    }
+
+                                    progressDialog.dismiss();
+
+
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+
+//                                    progressDialog.dismiss();
+                                    e.printStackTrace();
+                                    Toast.makeText(SignUpActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+
+
+                }else{
+                    Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show();
+                }
+
 
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
@@ -186,7 +306,6 @@ public class SignUpActivity extends AppCompatActivity {
                     if (task.isSuccessful()) {
 
                         Toast.makeText(SignUpActivity.this, "Picture uploaded successfully", Toast.LENGTH_SHORT).show();
-                        profilePicture.setImageURI(filePath2);
                     }
 
                 }
